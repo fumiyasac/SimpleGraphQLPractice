5d227ff9054a53d60d52cff680dafac1f56f35c6
fumiyasac <just1factory@gmail.com>
2023-05-22 14:23:23 +0900

    initial commit

---
+++ SimpleGraphQLPractice/SimpleGraphQLPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGraphQLPracticeApp.swift
+//  SimpleGraphQLPractice
+//
+//  Created by 酒井文也 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGraphQLPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
