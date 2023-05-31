4ae749138e3ddd3aa1b12ae99e85d81b7779ea62
fumiyasac <just1factory@gmail.com>
2023-05-31 23:49:20 +0900

    新規View追加＆Packageの削除

---
+++ SimpleGraphQLPractice/CountryDetail/CountryDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  CountryDetailView.swift
+//  SimpleGraphQLPractice
+//
+//  Created by 酒井文也 on 2023/05/31.
+//
+
+import SwiftUI
+
+struct CountryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryDetailView()
+    }
+}
---
+++ SimpleGraphQLPractice/CountryList/CountryListView.swift
@@ -0,0 +1,20 @@
+//
+//  CountryListView.swift
+//  SimpleGraphQLPractice
+//
+//  Created by 酒井文也 on 2023/05/31.
+//
+
+import SwiftUI
+
+struct CountryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryListView()
+    }
+}
