476cd5450290e88f43cc1d49dfee6a210be71b52
fumiyasac <just1factory@gmail.com>
2023-06-15 22:57:36 +0900

    CountryListRowの新規作成

---
+++ SimpleGraphQLPractice/CountryList/CountryListRow.swift
@@ -0,0 +1,20 @@
+//
+//  CountryListRow.swift
+//  SimpleGraphQLPractice
+//
+//  Created by 酒井文也 on 2023/06/15.
+//
+
+import SwiftUI
+
+struct CountryListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountryListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryListRow()
+    }
+}
