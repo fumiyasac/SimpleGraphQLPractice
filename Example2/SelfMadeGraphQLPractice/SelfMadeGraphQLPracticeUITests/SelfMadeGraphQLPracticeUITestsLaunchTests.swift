72158dc181e345f205ea9b8764880612c88100fc
fumiyasac <just1factory@gmail.com>
2023-07-15 21:36:45 +0900

    もう一つExample2を新規作成

---
+++ Example2/SelfMadeGraphQLPractice/SelfMadeGraphQLPracticeUITests/SelfMadeGraphQLPracticeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SelfMadeGraphQLPracticeUITestsLaunchTests.swift
+//  SelfMadeGraphQLPracticeUITests
+//
+//  Created by 酒井文也 on 2023/07/15.
+//
+
+import XCTest
+
+final class SelfMadeGraphQLPracticeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
