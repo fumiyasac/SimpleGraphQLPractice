fc34e1dbd1ecb969efb347eb48f8b4a200084396
fumiyasac <just1factory@gmail.com>
2023-08-22 09:46:49 +0900

    CombineExpectationsを利用したUnitTestに書き換える

--- Example1/SimpleGraphQLPracticeTests/CountryListViewModelTest.swift
+++ Example1/SimpleGraphQLPracticeTests/CountryListViewModelTest.swift
@@ -8,6 +8,7 @@
 @testable import SimpleGraphQLPractice
 
 import Combine
+import CombineExpectations
 import Nimble
 import Quick
 import XCTest
@@ -29,16 +30,49 @@ final class CountryListViewModelTest: QuickSpec {
                 countryListRepository: CountryListRepositorySuccessMock()
             )
 
+            // MEMO: このサンプルでは`@Published`を利用しているViewModelクラスの変数を記録対象とする
+            var requestStatusRecorder: Recorder<RequestStatus, Never>!
+            var countryListEntitiesRecorder: Recorder<[CountryListEntity], Never>!
+
             context("GraphQLでのデータ取得処理が成功した場合") {
-                viewModel.fetchCountryList()
+
+                // テスト前＆テスト後の記録対象変数に関する準備
+                beforeEach {
+                    requestStatusRecorder = viewModel.$requestStatus.record()
+                    countryListEntitiesRecorder = viewModel.$countryListEntities.record()
+                }
+                afterEach {
+                    requestStatusRecorder = nil
+                    countryListEntitiesRecorder = nil
+                }
 
                 it("Seedデータで定義したレスポンス値が返却＆成功ステータスが返却されること") {
+
+                    // 対象のViewModel側の処理を実行する
+                    viewModel.fetchCountryList()
+                    // timeout部分で0.16秒後の変化を見る
+                    // 👉 `@Published`の記録結果を取得する際に「CombineExpectation」が提供している.waitを利用するが、self.current.wait(...)とする必要があった。
+                    let requestStatusRecorderResult = try! self.current.wait(
+                        for: requestStatusRecorder.availableElements,
+                        timeout: 0.16
+                    )
+                    let countryListEntitiesRecorderResult = try! self.current.wait(
+                        for: countryListEntitiesRecorder.availableElements,
+                        timeout: 0.16
+                    )
+                    // 値変化の変遷が意図した物であることを確認する
+                    expect(requestStatusRecorderResult).to(equal([.none, .requesting, .success]))
+                    expect(countryListEntitiesRecorderResult).to(equal([[], CountryListSeeder.default]))
+
+                    // MEMO: 最終的な結果のみが必要な場合は下記の様に記載してもOK
+                    /*
                     expect(viewModel.requestStatus)
                         .toEventually(equal(.success))
                     expect(viewModel.countryListEntities)
                         .toEventually(equal(CountryListSeeder.default))
                     expect(viewModel.countryListEntities.count)
                         .toEventually(equal(5))
+                    */
                 }
             }
         }
@@ -51,16 +85,50 @@ final class CountryListViewModelTest: QuickSpec {
             let viewModel: CountryListViewModel = CountryListViewModel(
                 countryListRepository: CountryListRepositoryFailureMock()
             )
+
+            // MEMO: このサンプルでは`@Published`を利用しているViewModelクラスの変数を記録対象とする
+            var requestStatusRecorder: Recorder<RequestStatus, Never>!
+            var countryListEntitiesRecorder: Recorder<[CountryListEntity], Never>!
+            
             context("GraphQLでのデータ取得処理が失敗した場合") {
-                viewModel.fetchCountryList()
+
+                // テスト前＆テスト後の記録対象変数に関する準備
+                beforeEach {
+                    requestStatusRecorder = viewModel.$requestStatus.record()
+                    countryListEntitiesRecorder = viewModel.$countryListEntities.record()
+                }
+                afterEach {
+                    requestStatusRecorder = nil
+                    countryListEntitiesRecorder = nil
+                }
 
                 it("空配列が返却＆失敗ステータスが返却されること") {
+
+                    // 対象のViewModel側の処理を実行する
+                    viewModel.fetchCountryList()
+                    // timeout部分で0.16秒後の変化を見る
+                    // 👉 `@Published`の記録結果を取得する際に「CombineExpectation」が提供している.waitを利用するが、self.current.wait(...)とする必要があった。
+                    let requestStatusRecorderResult = try! self.current.wait(
+                        for: requestStatusRecorder.availableElements,
+                        timeout: 0.16
+                    )
+                    let countryListEntitiesRecorderResult = try! self.current.wait(
+                        for: countryListEntitiesRecorder.availableElements,
+                        timeout: 0.16
+                    )
+                    // 値変化の変遷が意図した物であることを確認する
+                    expect(requestStatusRecorderResult).to(equal([.none, .requesting, .failure]))
+                    expect(countryListEntitiesRecorderResult).to(equal([[]]))
+
+                    // MEMO: 最終的な結果のみが必要な場合は下記の様に記載してもOK
+                    /*
                     expect(viewModel.requestStatus)
                         .toEventually(equal(.failure))
                     expect(viewModel.countryListEntities)
                         .toEventually(equal([]))
                     expect(viewModel.countryListEntities.count)
                         .toEventually(equal(0))
+                    */
                 }
             }
         }
