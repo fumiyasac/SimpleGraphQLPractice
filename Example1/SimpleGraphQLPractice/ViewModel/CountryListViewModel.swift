70b82e63ea35accbd2dcbb24c36ba08ae489fa7f
fumiyasac <just1factory@gmail.com>
2023-08-22 09:45:50 +0900

    デバッグコメント削除

--- Example1/SimpleGraphQLPractice/ViewModel/CountryListViewModel.swift
+++ Example1/SimpleGraphQLPractice/ViewModel/CountryListViewModel.swift
@@ -37,9 +37,9 @@ final class CountryListViewModel: ObservableObject {
             do {
                 // MEMO: async/awaitベースの処理で必要な値を取得し、その後`@Published`で定義した値を更新する
                 self.countryListEntities = try await self.countryListRepository.getAllCountries()
-                dump(self.countryListEntities)
                 self.requestStatus = .success
             } catch let error {
+                // MEMO: 本来ならばエラーハンドリング処理等を入れる必要がある
                 print("Fetch Country List Error: " + error.localizedDescription)
                 self.requestStatus = .failure
             }
