3e858bb843b34f4d39ec89f3e20b6544b042245f
fumiyasac <just1factory@gmail.com>
2023-09-01 13:22:58 +0900

    Info.plist項目追加＆Extensionファイルの新規追加

---
+++ Example2/SelfMadeGraphQLPractice/Client/ApolloClientExtension.swift
@@ -0,0 +1,71 @@
+//
+//  ApolloClientExtension.swift
+//  SelfMadeGraphQLPractice
+//
+//  Created by 酒井文也 on 2023/09/01.
+//
+
+import Apollo
+import Foundation
+
+// 実装の参考:
+// 「apollo-ios」内のIssue内のディスカッションで紹介されていたコードを参考にしています。
+// Apollo内で用意されているQuery & Mutationの実行処理をasync/awaitでラッピングしている。
+// https://github.com/apollographql/apollo-ios/issues/2216
+
+// MARK: - ApolloClient Extension
+
+//extension ApolloClient {
+//
+//    // GraphQLのQueryをする処理をasync/awaitの処理内で実行する
+//    @discardableResult
+//    func fetchAsync<Query: GraphQLQuery>(
+//        query: Query,
+//        cachePolicy: CachePolicy = .default,
+//        contextIdentifier: UUID? = nil,
+//        queue: DispatchQueue = .main
+//    ) async throws -> GraphQLResult<Query.Data> {
+//
+//        // MEMO: withCheckedThrowingContinuationでErrorをthrowする形にしています。
+//        return try await withCheckedThrowingContinuation { continuation in
+//            fetch(
+//                query: query,
+//                cachePolicy: cachePolicy,
+//                contextIdentifier: contextIdentifier,
+//                queue: queue
+//            ) { result in
+//                switch result {
+//                case .success(let value):
+//                    continuation.resume(returning: value)
+//                case .failure(let error):
+//                    continuation.resume(throwing: error)
+//                }
+//            }
+//        }
+//    }
+//
+//    // GraphQLのMutationをする処理をasync/awaitの処理内で実行する
+//    @discardableResult
+//    func performAsync<Mutation: GraphQLMutation>(
+//        mutation: Mutation,
+//        publishResultToStore: Bool = true,
+//        queue: DispatchQueue = .main
+//    ) async throws -> GraphQLResult<Mutation.Data> {
+//
+//        // MEMO: withCheckedThrowingContinuationでErrorをthrowする形にしています。
+//        return try await withCheckedThrowingContinuation { continuation in
+//            perform(
+//                mutation: mutation,
+//                publishResultToStore: publishResultToStore,
+//                queue: queue
+//            ) { result in
+//                switch result {
+//                case .success(let value):
+//                    continuation.resume(returning: value)
+//                case .failure(let error):
+//                    continuation.resume(throwing: error)
+//                }
+//            }
+//        }
+//    }
+//}
