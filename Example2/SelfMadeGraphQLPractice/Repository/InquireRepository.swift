0276aef522d3c3720633458627628ea6b9725a09
fumiyasac <just1factory@gmail.com>
2023-10-08 10:25:34 +0900

    InquireRepositoryの新規追加

--- Example2/SelfMadeGraphQLPractice/Repository/InquireRepository.swift
+++ Example2/SelfMadeGraphQLPractice/Repository/InquireRepository.swift
@@ -5,4 +5,56 @@
 //  Created by 酒井文也 on 2023/10/08.
 //
 
+import Apollo
 import Foundation
+
+protocol InquireRepository {
+    func addInquire(
+        title: String,
+        purpose: String,
+        text: String
+    ) async throws -> InquireEntity?
+}
+
+final class InquireRepositoryImpl: InquireRepository {
+
+    // MARK: - Property
+
+    private let inquireRequest: InquireRequest
+
+    // MARK: - Initializer
+
+    init(inquireRequest: InquireRequest = InquireRequestImpl()) {
+        self.inquireRequest = inquireRequest
+    }
+
+    // MARK: - Function
+
+    func addInquire(
+        title: String,
+        purpose: String, 
+        text: String
+    ) async throws -> InquireEntity? {
+        let result = try await inquireRequest.addResult(
+            title: title,
+            purpose: purpose,
+            text: text
+        )
+        return convertToEntities(result: result)
+    }
+
+    // MARK: - Private Function
+
+    private func convertToEntities(result: GraphQLResult<MenuExhibitionSchema.AddInquireMutation.Data>) -> InquireEntity? {
+        if let addInquire = result.data?.addInquire {
+            return InquireEntity(
+                id: addInquire.id,
+                title: addInquire.title,
+                purpose: addInquire.purpose,
+                text: addInquire.text
+            )
+        } else {
+            return nil
+        }
+    }
+}
